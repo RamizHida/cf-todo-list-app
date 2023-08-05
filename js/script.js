dd177ae994eac535b61e5239a4703a7ae2a4d5a0
Ramiz Hida <ramizhida00@gmail.com>
2023-08-05 13:23:12 +0200

    Implemented add, delete, crossout and rearrange functionality

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function item() {
+  let listCollection = $('#list');
+  let listItem = $('<li></li>');
+
+  let input = $('#input');
+  if (input.val() === '') {
+    return alert('You must write something');
+  }
+
+  // Add list item
+  listItem.append(input.val());
+  listCollection.append(listItem);
+
+  // Cross out list item
+  function crossOutItem() {
+    listItem.toggleClass('strike');
+  }
+
+  listItem.on('dblclick', crossOutItem);
+
+  // Delete list item
+  let deleteBtn = $('<deleteBtn></deleteBtn>');
+  deleteBtn.append(document.createTextNode('X'));
+  listItem.append(deleteBtn);
+
+  function deleteItem() {
+    listItem.addClass('delete');
+  }
+
+  deleteBtn.on('click', deleteItem);
+
+  // Change order of list items
+  $('#list').sortable();
+}
